7d713303fb6b05fc2bca7c8391efc3d36812fe5c
Saiyaswanthpasupuleti <saiyaswanths959@gmail.com>
2024-11-28 16:41:03 +0530

    new

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 // import Main from './28nov/28nov';
 // import Conditiom from './28nov/condition';
 // import Parent from './parent';
-import Count from './counter';
+import Count from './28nov/counter';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
